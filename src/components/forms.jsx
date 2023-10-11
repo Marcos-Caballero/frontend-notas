d572659d97a0c2489fb2e1fa95216c32f0f9ce9e
Marcos-Caballero <robayo1100@gmail.com>
2023-10-11 16:33:37 -0500

    ...2

--- src/components/forms.jsx
+++ src/components/forms.jsx
@@ -16,7 +16,7 @@ function Form({oldNota}) {
         let URL = ''
         let params = {}
         if(nota._id){
-            URL = 'https://backend-notas-five.vercel.app//api/notas/' + nota._id;
+            URL = 'https://backend-notas-five.vercel.app/api/notas/' + nota._id;
             params = {
                 method: 'PATCH',
                 body: JSON.stringify(nota),
@@ -25,7 +25,7 @@ function Form({oldNota}) {
                 }
             }
         }else{
-            URL = 'https://backend-notas-five.vercel.app//api/notas/'
+            URL = 'https://backend-notas-five.vercel.app/api/notas/'
             params = {
                 method: 'POST',
                 body: JSON.stringify(nota),
